66e80ccb0b5e2bc5745cbb50e321e54d13da4016
kwontwitch <33889084+kwontwitch@users.noreply.github.com>
2020-07-30 10:27:24 +0900

    Update blockchain.py
    def resolution_conflicts은  new_chain을 None으로 시작하지만 이후, 이를 다른 값을 바꿔주는 코드를 만나지 못함.
    때문에 원본의 if문은 실행될 수 없고, 유효한 블록체인으로 교체되지 못하므로 코드 수정.

--- blockchain.py
+++ blockchain.py
@@ -69,13 +69,12 @@ class Blockchain(object):
 			if response.status_code == 200:
 				length = response.json()['length']
 				chain = response.json()['chain']
+				
 
 				if length > max_length and self.valid_chain(chain):
 					max_length = length
-
-			if new_chain:
-				self.chain = new_chain
-				return True
+					self.chain = chain
+					return True
 
 			return False
 
